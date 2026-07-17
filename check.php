ebba39985fae6e63fc0b88b79b53b38abd003def
AsharfulHosen <ashrafulaliff7@gamil.com>
2026-07-18 03:28:36 +0600

    admin section is fully functional

--- check.php
+++
@@ -1,1 +0,0 @@
-<?php include "includes/db.php"; $s=oci_parse($conn,"SELECT DISTINCT NAME FROM USER_SOURCE WHERE TYPE='PROCEDURE'"); oci_execute($s); while($r=oci_fetch_assoc($s)) { echo $r["NAME"] . "\n"; } ?>
